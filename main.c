2b5a91392407710fa136a14469541762ea70d6e9
Yao-studying <3317754104@qq.com>
2024-11-09 10:32:51 +0800

    绘制界面

---
+++ main.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <graphics.h>
+#include <windows.h>
+int main()
+{
+    // 1.创建界面  width:450   height:875   单位:px(像素)
+    // 一个格子宽75,高125;整个界面可以同时显示6行7列
+
+    initgraph(450, 875);
+
+    // 根据
+
+    // 卡屏
+    Sleep(2000);
+
+    // 结束
+    return 0;
+}
